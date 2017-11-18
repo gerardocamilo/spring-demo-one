13922ffc0bb1261474a44ea9e50fa2449886ee3f
Gerardo Camilo <gerardo.camilo@gmail.com>
2017-11-18 17:27:04 -0400

    Add American Football coach

---
+++ src/com/gerardocamilo/springdemo/AmericanFootballCoach.java
@@ -0,0 +1,10 @@
+package com.gerardocamilo.springdemo;
+
+public class AmericanFootballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Throw 100 ballons";
+	}
+
+}
